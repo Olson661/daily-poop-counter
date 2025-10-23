529684e519a907412f8832705c28f237380bf375
Your Name <yourname@example.com>
2025-10-22 22:28:39 -0700

    savep

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let saveEl = document.getElementById(save-el)
-let countEl = document.getElementById(count-el)
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment(){
@@ -8,9 +8,9 @@ function increment(){
 }
 
 function save(){
-    let countstr = count + "-"
-    saveEl.textContent = countstr
+    let countstr = " " + count + " - "
+    saveEl.textContent += countstr
     countEl.textContent = 0
     count = 0
-    console.log(count)
+    
 }
